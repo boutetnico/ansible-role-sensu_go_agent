48b32090a4a76bdac38cbd69d6d0c3d36b379225
Nicolas Boutet <amd3002@gmail.com>
2020-09-15 13:19:30 +0200

    Add variable to configure sensu-agent user additional groups

--- molecule/shared/tests/test_role.py
+++ molecule/shared/tests/test_role.py
@@ -23,6 +23,13 @@ def test_extra_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
+@pytest.mark.parametrize('name', [
+  ('adm'),
+])
+def test_user_belongs_to_its_additional_groups(host, name):
+    user = host.user('sensu')
+    assert user.contains(name)
+
 
 @pytest.mark.parametrize('file,user,group,mode,content', [
   ('/etc/sensu/agent.yml', 'root', 'root', 0o644, 'backend-url'),
