c1f7ae62287d0042438c620f20ab381434ff568f
Nicolas Boutet <amd3002@gmail.com>
2023-04-30 09:40:51 +0200

    Adapt CI workflow for molecule 5 (#8)
    * Adapt CI workflow for molecule 5
    
    * Remove Debian 10 & Ubuntu 20.04 as they are failing tests

--- molecule/shared/tests/test_role.py
+++ molecule/shared/tests/test_role.py
@@ -5,36 +5,49 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('sensu-go-agent'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("sensu-go-agent"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('name', [
-  ('sysstat'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("sysstat"),
+    ],
+)
 def test_extra_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('name', [
-  ('adm'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("adm"),
+    ],
+)
 def test_user_belongs_to_its_additional_groups(host, name):
-    user = host.user('sensu')
+    user = host.user("sensu")
     assert name in user.groups
 
 
-@pytest.mark.parametrize('file,user,group,mode,content', [
-  ('/etc/sensu/agent.yml', 'root', 'root', 0o644, 'backend-url'),
-])
+@pytest.mark.parametrize(
+    "file,user,group,mode,content",
+    [
+        ("/etc/sensu/agent.yml", "root", "root", 0o644, "backend-url"),
+    ],
+)
 def test_agent_config_file_exist(host, file, user, group, mode, content):
     config = host.file(file)
     assert config.exists
